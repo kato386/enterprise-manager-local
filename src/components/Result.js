949dc0cf54997de14da2bad07b39593eda38c302
Çağatay Yusuf Ergüneş <69120641+kato386@users.noreply.github.com>
2023-02-03 14:39:12 +0300

    variable name change

--- src/components/Result.js
+++ src/components/Result.js
@@ -20,41 +20,45 @@ const Result = ({
   const { gatewayData, errorGateway, isPending2 } =
     gatewayClicked && useFetch(endPointUrl);
 
-  const { paymentFormNumberData, errorPaymentFormNumber, isPending3 } =
+  const { paymentFormNumberData, errorPaymentFormNumber, isPendingFormNumber } =
     paymentFormNumberClicked && useFetch(endPointUrl);
 
   const { productTypesData, productTypesError, isPending4 } =
     productTypesClicked && useFetch(endPointUrl); */
 
-  const { data: gatewayData, isPending, error } = useFetch(endPoint);
+  const {
+    data: gatewayData,
+    isPending: isPendingGateway,
+    error: errorGateway,
+  } = useFetch(endPoint);
 
   const {
     data: productNumData,
-    isPending: isPending1,
-    error: error1,
+    isPending: isPendingProductNum,
+    error: errorProductNum,
   } = useFetch(endPoint);
 
   const {
     data: paymentFormNumberData,
-    isPending: isPending3,
-    error: error2,
+    isPending: isPendingFormNumber,
+    error: errorFormNumber,
   } = useFetch(endPoint);
 
   const {
     data: productTypesData,
-    isPending: isPending4,
-    error: error4,
+    isPending: isPendingProductTypes,
+    error: errorProductTypes,
   } = useFetch(endPoint);
   return (
     <div>
-      {error /* ||
+      {errorGateway /* ||
         errorGateway ||
             errorPaymentFormNumber ||
         productTypesError) */ && (
         <div className="relative h-[650px] before:bg-gradient-to-r before:from-purple-600 before:to-purple-300 before:absolute before:inset-0 before:w-full before:h-full before:z-10">
           <div className="container flex z-20 absolute items-center h-full justify-center">
             <h4 className="text-white text-4xl">
-              {error} <br />
+              {errorGateway} <br />
               {/* {errorGateway} <br />
               {errorPaymentFormNumber} <br />
               {productTypesError} <br /> */}
@@ -62,16 +66,19 @@ const Result = ({
           </div>
         </div>
       )}
-      {!error && isPending && isPending1 && isPending3 && isPending4 && (
-        <Loading />
-      )}
-      {!error && !isPending && gatewayClicked && (
+      {!errorGateway &&
+        isPendingGateway &&
+        isPendingProductNum &&
+        isPendingFormNumber &&
+        isPendingProductTypes && <Loading />}
+      {!errorGateway && !isPendingGateway && gatewayClicked && (
         <ResultGateway data={gatewayData} />
       )}
 
-      {!error1 &&
-        !isPending1 &&
-        !isPending3 &&
+      {!errorProductNum &&
+        errorFormNumber &&
+        !isPendingProductNum &&
+        !isPendingFormNumber &&
         (productNumClicked || paymentFormNumberClicked) && (
           <Chart
             productNumClicked={productNumClicked}
@@ -80,7 +87,7 @@ const Result = ({
             paymentFormNumberData={paymentFormNumberData}
           />
         )}
-      {!error4 && !isPending4 && productTypesClicked && (
+      {!errorProductTypes && !isPendingProductTypes && productTypesClicked && (
         <ResultProduct data={productTypesData} />
       )}
     </div>
